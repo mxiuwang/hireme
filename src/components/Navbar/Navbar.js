a64c0570ffd1522bb16fb4e41f3ea4a413cd4e4e
mxiuwang <mxwang@ualberta.ca>
2025-04-16 10:54:31 -0600

    font and formatting changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,34 +15,36 @@ export const Navbar = () => {
     <div>
       <nav className="navbar">
         <div className="navbar-container">
-          <a className="navbar-logo" href="/">
+          <a className="navbar-title" href="/">
             Hi It's Michelle
           </a>
-          <div className="menu-icon" onClick={menuClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          <div className="navbar-options">
+            <div className="menu-icon" onClick={menuClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item" onClick={closeMobileMenu}>
+                <a className={"nav-links"} href="#home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item" onClick={closeMobileMenu}>
+                <a className={"nav-links"} href="#perks">
+                  Perks
+                </a>
+              </li>
+              <li className="nav-item" onClick={closeMobileMenu}>
+                <a className={"nav-links"} href="#experience">
+                  Experience
+                </a>
+              </li>
+              <li className="nav-item" onClick={closeMobileMenu}>
+                <a className={"nav-links"} href="#people">
+                  People
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item" onClick={closeMobileMenu}>
-              <a className={"nav-links"} href="#home">
-                Home
-              </a>
-            </li>
-            <li className="nav-item" onClick={closeMobileMenu}>
-              <a className={"nav-links"} href="#perks">
-                Perks
-              </a>
-            </li>
-            <li className="nav-item" onClick={closeMobileMenu}>
-              <a className={"nav-links"} href="#experience">
-                Experience
-              </a>
-            </li>
-            <li className="nav-item" onClick={closeMobileMenu}>
-              <a className={"nav-links"} href="#people">
-                People
-              </a>
-            </li>
-          </ul>
         </div>
       </nav>
     </div>
